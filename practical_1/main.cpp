a4dc364a207d1b04bdc3dd0aabb5f8a7f7b3911f
Alessio Williams Gava <marveel@hotmail.it>
2018-01-16 21:59:31 +0000

    main.cpp:paddle moving, not going out from screen. Ball bounces. Points shown on screen. Some bugs: speed of ball sometime skyrockets and ball sometimes goes trough the paddles. ^cr^lf Added a font to res/font

--- practical_1/main.cpp
+++ practical_1/main.cpp
@@ -18,6 +18,10 @@ CircleShape ball;
 RectangleShape paddles[2];
 Vector2f ballVelocity;
 bool server = false;
+Font font;
+Text text;
+int scoreLx = 0;
+int scoreRx = 0;
 
 void Reset() {
 	//Reset paddle position 
@@ -26,9 +30,17 @@ void Reset() {
 	//reset ball position
 	ball.setPosition(gameWidth /2, gameHeight /2);
 	ballVelocity = {(server ? 100.0f : -100.0f), 60.0f};
+	text.setString(std::to_string(scoreLx) + "\t:\t"+ std::to_string(scoreRx));
+	text.setPosition((gameWidth * 0.5f) - (text.getLocalBounds().width * 0.5f), 0);
 }
 
 void Load(){
+	//Load font-face from res dir
+	font.loadFromFile("res/fonts/ANUDI.ttf");
+	//set text element to use font
+	text.setFont(font);
+	//char size to 24 px
+	text.setCharacterSize(24);
 	//Set size and origin of paddles
 	for(auto &p : paddles){
 		p.setSize(paddleSize - Vector2f(3,3));
@@ -50,20 +62,45 @@ void Update(RenderWindow & window) {
 	const float by = ball.getPosition().y;
 	if(by > gameHeight) {
 		//bottom wall
-		ballVelocity.x = 1.1f;
-		ballVelocity.y = -1.1f;
+		ballVelocity.x *= 1.1f;
+		ballVelocity.y *= -1.1f;
 		ball.move(0,-10);
 	} else if(by < 0) {
 		//top wall
-		ballVelocity.x = 1.1f;
-		ballVelocity.y = -1.1f;
+		ballVelocity.x *= 1.1f;
+		ballVelocity.y *= -1.1f;
 		ball.move(0,10);
 	} else if(bx > gameWidth) {
 		//right wall
+		scoreLx ++;
 		Reset();
 	} else if(bx < 0) {
 		//left wall
+		scoreRx ++;
 		Reset();
+	} else if (
+	  //ball is inline or behind paddle
+	  bx < paddleSize.x +10 && 
+	  //AND ball is below top edge of paddle
+	  by > paddles[0].getPosition().y - (paddleSize.y * 0.5) && 
+	  //AND ball is above bottom edge of paddle
+	  by < paddles[0].getPosition().y +(paddleSize.y * 0.5)
+	  ) {
+		//bounce off left paddle	
+		ballVelocity.x *= -1.1f;
+		ballVelocity.y *= 1.1f;
+	} else if (
+	  //ball is inline or behind paddle
+	  bx > gameWidth - paddleSize.x -10 &&
+	  //AND ball is below top edge of paddle
+	  by > paddles[1].getPosition().y -(paddleSize.y * 0.5) &&
+	  //AND ball is above bottom edge of paddle
+	  by < paddles[1].getPosition().y + (paddleSize.y * 0.5)
+		) {
+		//bounce off the right paddle
+		ballVelocity.x *= -1.1f;
+		ballVelocity.y *= 1.1f;
+
 	}
 	
 	//check and consume events
@@ -80,15 +117,39 @@ void Update(RenderWindow & window) {
 		window.close();
 	}
 	
-	//handle paddle movement
-	float direction = 0.0f;
-	if(Keyboard::isKeyPressed(controls[0])) {
-		direction --;
+	//handle paddle movement Player
+	float directionLx = 0.0f;
+	float directionRx = 0.0f;
+	if(Keyboard::isKeyPressed(controls[0]) && (paddles[0].getPosition().y - paddleSize.y / 2) > 0) {
+		directionLx --;
 	}
-	if(Keyboard::isKeyPressed(controls[1])) {
-		direction ++;
+	if(Keyboard::isKeyPressed(controls[1]) && (paddles[0].getPosition().y + paddleSize.y / 2) < gameHeight) {
+		directionLx ++;
 	}
-	paddles[0].move(0, direction * paddleSpeed * dt);
+	if (Keyboard::isKeyPressed(controls[2]) && (paddles[1].getPosition().y - paddleSize.y / 2) > 0) {
+		directionRx --;
+	}
+	if (Keyboard::isKeyPressed(controls[3]) && (paddles[1].getPosition().y + paddleSize.y / 2) < gameHeight) {
+		directionRx ++;
+	}
+	
+	//handle paddle movement AI
+	//check if ball is over or under than paddle, then direction of paddle will be set accordingly
+	if (ball.getPosition().y < paddles[0].getPosition().y && (paddles[0].getPosition().y - paddleSize.y / 2) > 0) {
+		directionLx --;
+	} 
+	else if (ball.getPosition().y > paddles[0].getPosition().y && (paddles[0].getPosition().y + paddleSize.y / 2) < gameHeight) {
+		directionLx ++;
+	}
+	if (ball.getPosition().y < paddles[1].getPosition().y && (paddles[1].getPosition().y - paddleSize.y / 2) > 0) {
+		directionRx --;
+	}
+	else if (ball.getPosition().y > paddles[0].getPosition().y && (paddles[1].getPosition().y + paddleSize.y / 2) < gameHeight) {
+		directionRx ++;
+	}
+
+	paddles[0].move(0, directionLx * paddleSpeed * dt);
+	paddles[1].move(0, directionRx * paddleSpeed * dt);
 }
 
 void Render(RenderWindow &window) {
@@ -96,6 +157,7 @@ void Render(RenderWindow &window) {
 	window.draw(paddles[0]);
 	window.draw(paddles[1]);
 	window.draw(ball);
+	window.draw(text);
 }
 int main(){
 	RenderWindow window(VideoMode(gameWidth, gameHeight), "PONG");
